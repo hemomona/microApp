c46e710e73ead369db6ffa9b77df932f5f26ab7e
hemomona <18146856052@163.com>
2022-04-05 00:11:04 +0800

    dispatch email to feedback

--- main.py
+++ main.py
@@ -14,22 +14,40 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
+from plyer import email, storagepath, uniqueid
 
+app_version = '1.0.1'
 
 # Loading Multiple .kv files
-Builder.load_file('screens/feedback.kv')
+# Builder.load_file('screens/feedback.kv')
 Builder.load_file('screens/records.kv')
 Builder.load_file('screens/experiment.kv')
 
 
 # Declare MainScreen in kv
 class MainScreen(Screen):
-    pass
+    def goto_experiment(self):
+        self.manager.current = 'experiment'
+
+    def goto_records(self):
+        self.manager.current = 'records'
+
+    def goto_feedback(self):
+        app_path = storagepath.get_application_dir()
+        device_id = uniqueid.get_uid()
+        email.send(recipient='18146856052@163.com',
+                   subject='app test',
+                   text='应用版本: ' + app_version + '\n'
+                        '保存路径: ' + app_path + '\n'
+                        '设备ID: ' + device_id + '\n'
+                        '其它: \n',
+                   create_chooser=False)
 
 
 # Declare FeedbackScreen in kv
-class FeedbackScreen(Screen):
-    pass
+# FeedbackScreen has been discarded
+# class FeedbackScreen(Screen):
+#     pass
 
 
 # Declare RecordsScreen in kv
@@ -51,6 +69,7 @@ class MicroApp(App):
     def build(self):
         # Kivy supports only 1 window per application
         Window.size = (360, 792)
+        self.title = 'micro-GCFP'
         return
 
 
